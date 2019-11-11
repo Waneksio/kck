51b94799ae3c5110a365a34831317c28284d6ff2
Waneksio <waneksiomiszczu@gmail.com>
2019-11-11 20:29:01 +0100

    created image reader

---
+++ untitled/Source/ImReader.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import data, io, filters, exposure
+import skimage
+
+
+class ImReader:
+    image = []
+
+    def __init__(self, imagePath):
+        self.image = self.readImage(imagePath)
+
+    def readImage(self, imagePath):
+        return io.imread('notes.jpg')
+
+    def showImage(self):
+        plt.figure(figsize=(10, 10))
+        io.imshow(self.image)
+        plt.show()
+
+    def wbConvert(self):
+        for pixelArray in self.image:
+            for pixel in pixelArray:
+                if pixel > 200:
+                    pixel = 255
+                else:
+                    pixel = 0
+
+
+if __name__ == "__main__":
+    NewImage = ImReader("notes.jpg")
+    NewImage.showImage()
