9fb4330afd34019aa1bb46ec4cb0e91c86495f4b
Piotr <pppiotr98@gmail.com>
2019-11-22 12:28:49 +0100

    addRemoveLines

--- Source/LineDetector.py
+++ Source/LineDetector.py
@@ -7,6 +7,7 @@ class LineDetector:
     image = []
     lines = []
     linesImage = []
+    noLinesImage = []
 
     rhoAccuracy = 1
     thetaAccuracy = np.pi / 180
@@ -17,12 +18,13 @@ class LineDetector:
         self.image = img
         self.lines = self.findLines()
         self.linesImage = self.drawLines(self.image.image)
+        self.noLinesImage = self.removeLines(self.image.image)
 
     def findLines(self):
         return cv2.HoughLines(self.image.edges, self.rhoAccuracy, self.thetaAccuracy, self.treshold)
 
     def drawLines(self, img):
-        linesImage = img
+        linesImage = img.copy()
         for l in self.lines:
             for rho, theta in l:
                 a = np.cos(theta)
@@ -37,7 +39,28 @@ class LineDetector:
                 cv2.line(linesImage, (x1, y1), (x2, y2), self.linesColor, 2)
         return linesImage
 
+    def removeLines(self, img):
+        noLinesImage = img.copy()
+        for l in self.lines:
+            for rho, theta in l:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + self.image.imgWidth * (-b))
+                y1 = int(y0 + self.image.imgHeight * (a))
+                x2 = int(x0 - self.image.imgWidth * (-b))
+                y2 = int(y0 - self.image.imgHeight * (a))
+
+                cv2.line(noLinesImage, (x1, y1), (x2, y2), (255, 255, 255), 3)
+        return noLinesImage
+
     def showLinesImage(self):
         plt.figure(figsize=(10, 10))
         io.imshow(self.linesImage)
+        plt.show()
+
+    def showNoLinesImage(self):
+        plt.figure(figsize=(10, 10))
+        io.imshow(self.noLinesImage)
         plt.show()
--- Source/main.py
+++ Source/main.py
@@ -4,4 +4,5 @@ from LineDetector import LineDetector
 if __name__ == "__main__":
     newImage = ImReader('E:/moje/kodziki/python/notesReader/Resources/notes2.jpg')
     newDetector = LineDetector(newImage)
-    newDetector.showLinesImage()
+    newImage.showImage()
+    newDetector.showNoLinesImage()
