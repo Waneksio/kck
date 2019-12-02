e78c620939fc7cb89011623e8fe48c80a7b47f6b
Piotr <pppiotr98@gmail.com>
2019-12-02 13:02:54 +0100

    Image filter update

--- Source/main.py
+++ Source/main.py
@@ -5,5 +5,20 @@ from ShapeDetector import *
 if __name__ == "__main__":
 
     NewImage = ImReader("../Resources/medium/m1.jpg")
-    NewImage.showBinImage()
-
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m2.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m3.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m4.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m5.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m6.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m7.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m8.jpg")
+    NewImage.showEdges()
+    NewImage = ImReader("../Resources/medium/m9.jpg")
+    NewImage.showEdges()
